ce25192b3a344d59c1d66df6f776d7c1d6775e9c
HesterSho <lolittasisi123@gmail.com>
2025-09-06 15:27:47 +0100

    modify

---
+++ dashboard/dashboard.js
@@ -0,0 +1,9 @@
+document.getElementById("demand").addEventListener("click", function() {
+    window.location.href ="../requests/demand.html";
+}
+)
+
+document.getElementById("leaves").addEventListener("click", function() {
+    window.location.href ="../myleaves/leaves.html";
+}
+)
